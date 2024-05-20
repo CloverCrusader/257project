8996b32de48b1cd5b1339d68b91d071ab699ca9a
michlamm <165341285+michlamm@users.noreply.github.com>
2024-05-20 10:47:02 -0500

    Update compare.py

--- compare.py
+++ compare.py
@@ -15,7 +15,7 @@ def get_name_options():
     database="rapaczs", # your sterns account
     user="rapaczs", # your sterns usernam
     password="chip979bond" # your low security password
-  
+ 
  cur = conn.cursor()
 
     query = "SELECT * FROM schoolstats ORDER BY school ASC";
