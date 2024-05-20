9375097f09f97738e804ca3045f0308dbc8e47bc
FridaRojo <165401736+FridaRojo@users.noreply.github.com>
2024-05-20 17:43:01 -0500

    added new pg

--- compare.py
+++ compare.py
@@ -5,9 +5,6 @@ import psycopg2;
 import json;
 app = Flask(__name__)
 
-
-
-
 def get_name_options():
 
   conn = psycopg2.connect(
@@ -37,50 +34,11 @@ def comparepg():
 
   html_string = get_name_options()
   return render_template("compare.html", DropdownOptions = html_string)
-
   
-
-#     html = ""
-#     for row in rows:
-#         last = row[0]
-#         first = row[1]
-
-#         #Here is more info on Python's Formatted Strings
-#         #    https://docs.python.org/3/tutorial/inputoutput.html
-#         html = html + f'<option value="{first} {last}">{last}</option>'
-
-#         #Backslash n in a string means New Line
-#         html = html + '\n'
-    
-#     return html
-
-
-
-# @app.route('/')
-# def welcome():
-
-#     html_string = get_name_options()
-
-
-#     # IMPORTANT: The only reason that this works is because
-#     #   In the Template, I marked DropdownOptions as "Safe"
-#     #   This tells Flasks that it is safe to insert HTML code into that
-#     #   location in the template
-#     return render_template("homepage.html", DropdownOptions = html_string)
-
-
-# @app.route('/insert/<first_name>/<last_name>/')
-# def insert_name(first_name,last_name):
-
-#     conn = psycopg2.connect(
-#         host="localhost",
-#         port=5432,
-#         database="mlepinski",
-#         user="mlepinski",
-#         password="nunuiscute")
-    
-#     cur = conn.cursor()
-
+@app.route('comparing')
+def comparingStats():
+  return render_template("comparingStats.htm")
+  
 
 if __name__ == '__main__':
     my_port = 5223
