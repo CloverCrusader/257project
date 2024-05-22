b2d7aa549a4e7b567843b2f80373e12c28a5e6f9
AimeeJade <126742356+AimeeJade@users.noreply.github.com>
2024-05-22 16:20:55 -0500

    Update aimee-test.py

--- aimee-test.py
+++ aimee-test.py
@@ -4,11 +4,24 @@ import psycopg2;
 import json;
 app = Flask(__name__)
 
+@app.route('/')
+def aimee_test_pg():
+  html_string = get_name_options()
+  return render_template("aimee-test.html", DropdownOptions = html_string)
+
+@app.route('/aimeeStats')
+def aimee_stats():
+    income = request.args.get('income')
+    colleges = request.args.get('colleges')
+  
+    stats = get_college_stats(income, colleges)
+    return render_template("aimee-test.html", stats=stats)
+    
 def get_name_options():
 
   conn = psycopg2.connect(
         host="localhost",
-        port=5432,
+        port=5232,
         database="rapaczs",
         user="rapaczs",
         password="chip979bond")
@@ -27,19 +40,34 @@ def get_name_options():
   return html
 
 @app.route('/')
-def aimee_test_pg():
+def compare_pg():
   html_string = get_name_options()
-  return render_template("aimee-test.html", DropdownOptions = html_string)
+  return render_template("compare.html", DropdownOptions = html_string)
 
-@app.route('/aimeeStats')
-def aimee_stats():
-    income = request.args.get('income')
-    colleges = request.args.get('colleges')
-  
-    stats = get_college_stats(income, colleges)
-    return render_template("aimee-test.html", stats=stats)
+def get_college_stats(college1, college2):
+    conn = psycopg2.connect(
+        host="localhost",
+        port=5232,
+        database="rapaczs",
+        user="rapaczs",
+        password="chip979bond")
+    cur = conn.cursor()
     
+    query = """
+        SELECT * FROM schoolstats WHERE school IN (%s, %s)
+    """
+    cur.execute(query, (college1, college2))
+    results = cur.fetchall()
+    conn.close()
+    return results
 
+@app.route('/comparingStats')
+def comparing_stats():
+    college1 = request.args.get('college1')
+    college2 = request.args.get('college2')
+  
+    stats = get_college_stats(college1, college2)
+    return render_template("comparingStats.html", stats=stats)
 
 if __name__ == '__main__':
     my_port = 5232
