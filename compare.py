d4a31059b0140ffe4a6cd90f974e6f8a48c3a44a
FridaRojo <165401736+FridaRojo@users.noreply.github.com>
2024-05-22 13:29:03 -0500

    did some things, no idea if then work tho

--- compare.py
+++ compare.py
@@ -13,12 +13,10 @@ def get_name_options():
         database="rapaczs",
         user="rapaczs",
         password="chip979bond")
-
   cur = conn.cursor()
 
   query = "SELECT school FROM schoolstats ORDER BY school ASC";
   cur.execute(query)
-    
   rows = cur.fetchall()
 
   html= ""
@@ -27,20 +25,39 @@ def get_name_options():
 
     html = html + f'<option value="{school}">{school}</option>'
     html = html + '\n'
-  
   return html
 
 @app.route('/')
-def comparepg():
-
+def compare_pg():
   html_string = get_name_options()
   return render_template("compare.html", DropdownOptions = html_string)
-  
+
+def get_college_stats(college1, college2):
+    conn = psycopg2.connect(
+        host="localhost",
+        port=5432,
+        database="rapaczs",
+        user="rapaczs",
+        password="chip979bond")
+    
+  query = """
+        SELECT * FROM college_stats
+        WHERE college_name IN (%s, %s)
+    """
+    cursor.execute(query, (college1, college2))
+    results = cursor.fetchall()
+    conn.close()
+    return results
+
 @app.route('/comparingStats')
-def comparingStats():
-  
-  return render_template("comparingStats.html")
-  
+def comparing_stats():
+    college1 = request.args.get('college1')
+    college2 = request.args.get('college2')
+    if college1 and college2:
+        stats = get_college_stats(college1, college2)
+        return render_template("comparingStats.html", stats=stats)
+    return render_template("comparingStats.html", stats=None)
+
 
 if __name__ == '__main__':
     my_port = 5223
