ebbcfb421084a5f39d03d413d89c57d00348aec3
AimeeJade <126742356+AimeeJade@users.noreply.github.com>
2024-05-22 20:05:54 -0500

    Update aimee-test.py

--- aimee-test.py
+++ aimee-test.py
@@ -38,37 +38,6 @@ def get_name_options():
     html = html + f'<option value="{school}">{school}</option>'
     html = html + '\n'
   return html
-"""
-@app.route('/')
-def compare_pg():
-  html_string = get_name_options()
-  return render_template("compare.html", DropdownOptions = html_string)
-
-def get_college_stats(college1, college2):
-    conn = psycopg2.connect(
-        host="localhost",
-        port=5232,
-        database="rapaczs",
-        user="rapaczs",
-        password="chip979bond")
-    cur = conn.cursor()
-    
-    query = 
-        "SELECT * FROM schoolstats WHERE school IN (%s, %s);"
-    
-    cur.execute(query, (college1, college2))
-    results = cur.fetchall()
-    conn.close()
-    return results
-
-@app.route('/comparingStats')
-def comparing_stats():
-    college1 = request.args.get('college1')
-    college2 = request.args.get('college2')
-  
-    stats = get_college_stats(college1, college2)
-    return render_template("comparingStats.html", stats=stats)
-    """
 
 if __name__ == '__main__':
     my_port = 5232
