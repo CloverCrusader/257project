8b36a36c7b032d58f733e73253325c8b4b14aef4
AimeeJade <126742356+AimeeJade@users.noreply.github.com>
2024-05-23 23:27:45 -0500

    Update aimee-test.py

--- aimee-test.py
+++ aimee-test.py
@@ -50,7 +50,7 @@ def get_college_stats(income, colleges):
 @app.route('/financialAid/<income>/<colleges>')
 def comparing_stats(income, colleges):
     stats = get_college_stats(income, colleges)
-    return render_template("popularMajor.html", stats=stats)
+    return render_template("financialAid.html", stats=stats)
 
 
 def get_major_stats(major):
