49a31be31d8d56a4eaad4ecc114d1ae12a31d133
CloverCrusader <80260043+CloverCrusader@users.noreply.github.com>
2024-06-02 16:19:36 -0500

    Update collegeFind.py

--- collegeFind.py
+++ collegeFind.py
@@ -3,6 +3,11 @@ import psycopg2
 
 app = flask.Flask(__name__)
 
+
+'''
+Executes a pqsl query to a database.
+Returns an array of school names.
+'''
 def get_name_options():
   conn = psycopg2.connect(
         host="localhost",
@@ -26,12 +31,21 @@ def get_name_options():
   
 # Home functionality
 
+
+'''
+Runs homepage.
+'''
 @app.route('/')
 def home():
     return flask.render_template("finalhome.html")
-  
-# Ranking functionality
 
+
+'''
+Executes a pqsl query to a database.
+'rate' - specifies statistic of interest
+'lowhigh' - designates ascending of descending sort order
+Returns an array of sorted schools.
+'''
 def get_ranking_stats(rate, lowhigh):
     conn = psycopg2.connect(
         host="localhost",
@@ -48,6 +62,10 @@ def get_ranking_stats(rate, lowhigh):
     
     return results
 
+
+'''
+Displays results of a ranking query.
+'''
 @app.route('/rankings/<rate>/<lowhigh>')
 def displayRanking(rate, lowhigh):
     titleOptions = {'acceptrate' : 'Acceptance Rate:', 'gradrate' : 'Graduation Rate:', 'tuition' : 'Average Tuition:'}
@@ -62,13 +80,22 @@ def displayRanking(rate, lowhigh):
   
     return flask.render_template("display-rankings.html", stats=stats, rate=rate, title=titleOptions[rate], head=head, tail=tail)
 
+
+'''
+Runs Rankings query page.
+'''
 @app.route('/rankings')
 def rankings():
     return flask.render_template("finalrankings.html")
 
 
 # Compare functionality
-
+'''
+Executes a pqsl query to a database.
+'college2' - designates first college
+'college1' - designates second college
+Returns an array of two schools' data.
+'''
 def get_college_stats(college1, college2):
     conn = psycopg2.connect(
         host="localhost",
@@ -84,18 +111,34 @@ def get_college_stats(college1, college2):
     conn.close()
   
     return results
-  
+
+
+'''
+Displays results of a comparison query.
+'''
 @app.route('/compare/<college1>/<college2>')
 def comparing_stats(college1, college2):
     stats = get_college_stats(college1, college2)
     return flask.render_template("display-compare.html", stats=stats)
-    
+
+
+'''
+Runs Compare query page.
+'''
 @app.route('/compare')
 def compare():
     dropdownOptions = get_name_options()
     return flask.render_template("finalcompare.html", DropdownOptions = dropdownOptions)
-  
+
+
 # Financial Aid functionality
+
+'''
+Executes a pqsl query to a database.
+'income' - specifies income bracket
+'colleges' - specifies college to query financial aid
+Returns a single monetary value.
+'''
 def get_aid_stats(income, colleges):#colleges
     conn = psycopg2.connect(
         host="localhost",
@@ -111,19 +154,33 @@ def get_aid_stats(income, colleges):#colleges
     conn.close()
     return aid
 
+
+'''
+Displays the results of a financial aid query.
+'''
 @app.route('/financialaid/<income>/<colleges>')
 def comparing_aidStats(income, colleges):
     aid = get_aid_stats(income, colleges) #colleges
     
     return flask.render_template("display-aid.html", aid=aid, colleges=colleges)
-  
+
+
+'''
+Runs Financial Aid query page.
+'''
 @app.route('/financialaid')
 def financialAid():
     dropdownOptions = get_name_options()
     return flask.render_template("finalfinancial-aid.html",  DropdownOptions = dropdownOptions)
-  
-# Popular Major functionality
 
+
+
+# Popular Major functionality
+'''
+Executes a pqsl query to a database.
+'major' - specifies value to search for matches
+Returns an array of schools with compatible popular major.
+'''
 def get_major_stats(major):
     conn = psycopg2.connect(
         host="localhost",
@@ -140,20 +197,31 @@ def get_major_stats(major):
 
     return results
 
+
+'''
+Displays the results of a popular major query
+'''
 @app.route('/popularmajor/<major>')
 def displayMajors(major):
     stats = get_major_stats(major)
-  
-    return flask.render_template("display-major.html", stats=stats, major=major) # rename 
-  
+    return flask.render_template("display-major.html", stats=stats, major=major)
+
+
+'''
+Runs Popular Major query page.
+'''
 @app.route('/popularmajor')
 def popMajor():
     return flask.render_template("finalpopular-major.html")
-  
+
+'''
+Runs map page.
+'''
 @app.route('/map')
 def map():
     return flask.render_template("display-map.html")
-  
+
+
 if __name__ == '__main__':
     my_port = 5223
     app.run(host = '0.0.0.0', port = my_port)
